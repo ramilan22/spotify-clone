ec380962705ee54f4bc7f1d78b9657d3e0af557c
ramibabay <ramibabay21@gmail.com>
2023-06-23 23:37:07 +0100

    final

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains:[
+            "nweubhtgeoqarmrzglsf.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
